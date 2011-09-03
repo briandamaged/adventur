69aa4687b1c145f68d094fccbaaac817b508c545
Brian Lauber <constructible.truth@gmail.com>
2011-09-03 18:10:42 -0400

    Initial commit.  Empty rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Adventur::Application.config.secret_token = '7b9c3109005132833ee4fb132739b2061e9a6decb0c2351f089e336a47c133317f5617b162b5d71d1dba8324e96114f8a060700d0a4a1ce1f0914c6c10512e9e'
