4e7879fd788019dc355144ea2e9a5ea1f9aefb6e
Brian Lauber <constructible.truth@gmail.com>
2011-09-03 20:32:31 -0400

    Got image upload prototype working, but I think I'm going to use the paperclip plugin instead

--- app/models/scene.rb
+++ app/models/scene.rb
@@ -1,5 +1,21 @@
 class Scene < ActiveRecord::Base
-  validates_presence_of :name, :description
+  attr_accessor         :image
 
+  before_save           :save_image
+  
+  validates_presence_of :name, :description
+  validates_presence_of :image, :on => :create
+
+
+
+
+  private
+  
+  def save_image()
+    if self.image.present?
+      self.image_path = File.join("public", "data", "images", @image.original_filename)
+      File.open(self.image_path, "wb") {|f| f.write(@image.read) }
+    end
+  end
 
 end
